ce8a40b3e634e547f5295ea569caf80777340f45
Eric Ung <ungericw@tutanota.com>
2022-11-02 17:56:35 -0500

    Interface and models for services

---
+++ Mango.Web/Services/IServices/IProductServices.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductServices
+    {
+        Task<T> GetAllProductsAsync<T>();
+
+        Task<T> GetProductByIdAsync<T>(int id);
+
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+
+        Task<T> DelteProductAsync<T>(int id);
+    }
+}
