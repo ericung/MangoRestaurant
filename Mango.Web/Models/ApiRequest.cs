c079cb7fbeb62739503f6f71e0bbc7b5ed5df658
Eric Ung <ungericw@tutanota.com>
2022-11-02 18:07:20 -0500

    BaseService

--- Mango.Web/Models/ApiRequest.cs
+++ Mango.Web/Models/ApiRequest.cs
@@ -4,7 +4,7 @@ namespace Mango.Web.Models
 {
     public class ApiRequest
     {
-        public ApiType apiType { get; set; } = ApiType.GET;
+        public ApiType ApiType { get; set; } = ApiType.GET;
 
         public string Url { get; set; }
 
