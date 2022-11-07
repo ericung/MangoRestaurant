56bd476f541112c57f7cfc0053e642e2082a2763
Eric Ung <ungericw@tutanota.com>
2022-11-06 18:09:15 -0600

    Product Nav

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new List<ProductDto>();
+            var response = await productService.GetAllProductsAsync<ResponseDto>();
+            if (response!= null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
