8845c1404ba34170d9bb4c9c8ac783a959fa58a2
Eric Ung <ungericw@tutanota.com>
2022-11-06 18:16:30 -0600

    fix productapi

--- Mango.Services.ProductAPI/Models/Dto/ResponseDto.cs
+++ Mango.Services.ProductAPI/Models/Dto/ResponseDto.cs
@@ -2,7 +2,7 @@
 {
     public class ResponseDto
     {
-        public bool IsSuccess { get; set; }
+        public bool IsSuccess { get; set; } = true;
 
         public object Result { get; set; }
 
