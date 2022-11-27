50a6c2bd14073b06149959f1c4d5cd1cffc8c8bf
Eric Ung <ungericw@tutanota.com>
2022-11-27 13:02:44 -0600

    pass user claims

---
+++ Mango.Services.Identity/Services/ProfileService.cs
@@ -0,0 +1,63 @@
+﻿using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Mango.Services.Identity.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IUserClaimsPrincipalFactory<ApplicationUser> userClaimsPrincipalFactory;
+
+        public ProfileService(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IUserClaimsPrincipalFactory<ApplicationUser> userClaimsPrincipalFactory)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.userClaimsPrincipalFactory = userClaimsPrincipalFactory;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            string sub = context.Subject.GetSubjectId();
+            ApplicationUser user = await userManager.FindByNameAsync(sub);
+            ClaimsPrincipal userClaims = await userClaimsPrincipalFactory.CreateAsync(user);
+
+            List<Claim> claims = userClaims.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+
+            if (userManager.SupportsUserRole)
+            {
+                IList<string> roles = await userManager.GetRolesAsync(user);
+                foreach(var rolename in roles)
+                {
+                    claims.Add(new Claim(JwtClaimTypes.Role, rolename));
+                    claims.Add(new Claim(JwtClaimTypes.FamilyName, user.LastName));
+                    claims.Add(new Claim(JwtClaimTypes.GivenName, user.FirstName));
+                    if (roleManager.SupportsRoleClaims)
+                    {
+                        IdentityRole role = await roleManager.FindByNameAsync(rolename);
+                        if (role is object)
+                        {
+                            claims.AddRange(await roleManager.GetClaimsAsync(role));
+                        }
+                    }
+                }
+            }
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            string sub = context.Subject.GetSubjectId();
+            ApplicationUser user = await userManager.FindByIdAsync(sub);
+            context.IsActive = user != null;
+        }
+    }
+}
